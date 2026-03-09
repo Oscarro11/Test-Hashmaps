628233ed1133a8ac9d63585a70a27dae47ae8e3b
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-03-08 18:29:48 -0600

    feat: created ProductoTaco model class

---
+++ src/main/ProductoTaco.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.lang.Math;
+import java.time.LocalDate;
+
+public class ProductoTaco implements IFuncionHash{
+    private String nombre;
+    private String tipo;
+    private LocalDate fechaVenc;
+
+    public ProductoTaco(String nombre, String tipo, LocalDate fechaVenc){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.fechaVenc = fechaVenc;
+    }
+
+    public ProductoTaco(String tipo, LocalDate fechaVenc){
+        this.tipo = tipo;
+        this.fechaVenc = fechaVenc;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public LocalDate getFechaVenc(){
+        return fechaVenc;
+    }
+
+    @Override
+    public int generarHash(){
+        char[] arregloTipo = tipo.toCharArray();
+        int valor = 0;
+
+        for (int i = 0; i < tipo.length(); i++) {
+            valor += Math.pow((int) arregloTipo[i], i+1);
+        };
+
+        valor = (valor + fechaVenc.getDayOfMonth()) * fechaVenc.getMonthValue() - fechaVenc.getYear();
+        return valor % 20;
+    }
+}
